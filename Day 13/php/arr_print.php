7d653c5d3055e746f932114ac664ed0af34ddf71
DHOKIYA DEEPKUMAR <137034543+deepkumar1805@users.noreply.github.com>
2023-07-04 15:46:55 +0530

    Add files via upload

---
+++ Day 13/php/arr_print.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$animal = array("dog", "cat", "cow"); 
+echo "I like " . $animal[0] . ", " . $animal[1] . " and " . $animal[2] . "."; 
+print "I like " . $animal[0] . ", " . $animal[1] . " and " . $animal[2] . ".";
+?>
+
+</body>
+</html>
---
+++ Day 13/php/asso_arr.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$age = array("john"=>"44", "zoro"=>"16", "simon"=>"23");
+echo "john is " . $age['john'] . " years old.";
+?>
+
+</body>
+</html>
